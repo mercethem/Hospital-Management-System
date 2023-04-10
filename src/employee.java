956ab34c107b700071244f510e92bc39f8294d54
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-11 01:31:25 +0300

    employee class sketch added

---
+++ src/employee.java
@@ -0,0 +1,11 @@
+public class employee extends person {
+
+    private long employeeId;
+    private String department;
+    private int dateOfStart;
+    private int dismissalDate;
+
+    public employee(){
+        System.out.println("New employee created.");
+    }
+}
---
+++ src/patient.java
@@ -0,0 +1,12 @@
+public class patient extends person {
+
+    private String department;
+    private int hospitalizedDate;
+    private int leaveDate;
+
+
+
+    public patient(){
+        System.out.println("New patient created.");
+    }
+}
