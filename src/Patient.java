c71194e34c967b426e94865168a10718caec06fc
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-13 03:54:25 +0300

    class name changed

---
+++ src/Patient.java
@@ -0,0 +1,10 @@
+public class Patient extends Person {
+
+    private String department;
+    private int hospitalizedDate;
+    private int leaveDate;
+
+    public Patient(){
+        System.out.println("New Patient created.");
+    }
+}
