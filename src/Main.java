5e3aa3f8e442662af7d56c6fa359d5d6791cbff5
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-13 03:54:06 +0300

    test added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    person ahmet = new person();
+        Add.addPerson();
+        Person person1 = new Person();
+        person1.setName("Ethem");
     }
 }
