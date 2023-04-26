0efac0741e5bac1dea06c1dbef6d48e52e1397d0
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-26 15:56:38 +0300

    test changed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Menu.options();
-        System.out.println(Employee.employee);
+        Employee.employee.viewPerson();
+
     }
 }
