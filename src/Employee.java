3dc99ca35573155cfc053595832564e0c5a79e88
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-26 14:45:45 +0300

    constructors added,toString, view method, change, methods added

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,22 @@
 import java.awt.*;
 import java.time.Year;
+import java.util.Scanner;
+
 
 public class Employee extends Person {
+    static Employee employee = new Employee("220541022", "Software Engineering",
+            1, 1, 2022, 0,0,2023);
+
+    Scanner keyboard = new Scanner(System.in);
     final private int EMPLOYEE_ID_DIGIT = 11;
     private String employeeId;
     private String department;
     private int dateOfStart_day;
     private int dateOfStart_month;
     private int dateOfStart_year;
-    private int dismissalDate_day;
-    private int dismissalDate_month;
-    private int dismissalDate_year;
+    private int dateOfDismiss_day;
+    private int dateOfDismiss_month;
+    private int dateOfDismiss_year;
 
 
     public String getEmployeeId() {
@@ -62,10 +68,6 @@ public class Employee extends Person {
             setDepartment("");
         }
 
-    }
-
-
-    public Employee() {
     }
 
     public int getDateOfStart_day() {
@@ -156,90 +158,167 @@ public class Employee extends Person {
 
     }
 
-    public int dismissalDate_day() {
-        return dismissalDate_day;
+    public int getDateOfDismiss_day() {
+        return dateOfDismiss_day;
     }
 
-    public void setDismissalDate_day(int dismissalDate_day) {
+    public void setDateOfDismiss_day(int dateOfDismiss_day) {
         try {
             System.out.println("Please enter date of dismissal day : ");
             String sDay = keyboard.nextLine();
-            dismissalDate_day = Integer.parseInt(sDay);
+            dateOfDismiss_day = Integer.parseInt(sDay);
             if (!sDay.matches(".*[0-9].*")) {
                 System.out.println("Please use just digits.");
                 Toolkit.getDefaultToolkit().beep();
-                setDismissalDate_day(0);
-            } else if (dismissalDate_day < 0) {
+                setDateOfDismiss_day(0);
+            } else if (dateOfDismiss_day < 0) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_day = 1;
-            } else if (dismissalDate_day >= 31) {
+                this.dateOfDismiss_day = 1;
+            } else if (dateOfDismiss_day >= 31) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_day = 31;
+                this.dateOfDismiss_day = 31;
             } else {
-                this.dismissalDate_day = dismissalDate_day;
+                this.dateOfDismiss_day = dateOfDismiss_day;
             }
         } catch (Exception e) {
             System.out.println("Please enter just digit!");
             Toolkit.getDefaultToolkit().beep();
-            setDismissalDate_day(0);
+            setDateOfDismiss_day(0);
         }
     }
 
-    public int getDismissalDate_month() {
-        return dismissalDate_month;
+    public int getDateOfDismiss_month() {
+        return dateOfDismiss_month;
     }
 
-    public void setDismissalDate_month(int dismissalDate_month) {
+    public void setDateOfDismiss_month(int dateOfDismiss_month) {
         try {
             System.out.println("Please enter date of dismissal month : ");
             String sMonth = keyboard.nextLine();
-            dismissalDate_month = Integer.parseInt(sMonth);
+            dateOfDismiss_month = Integer.parseInt(sMonth);
             if (!sMonth.matches(".*[0-9].*")) {
                 System.out.println("Please use just digits.");
                 Toolkit.getDefaultToolkit().beep();
-                setDismissalDate_month(1);
-            } else if (dismissalDate_month < 0) {
+                setDateOfDismiss_month(1);
+            } else if (dateOfDismiss_month < 0) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_month = 1;
-            } else if (dismissalDate_month >= 12) {
+                this.dateOfDismiss_month = 1;
+            } else if (dateOfDismiss_month >= 12) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_month = 12;
+                this.dateOfDismiss_month = 12;
             } else {
-                this.dismissalDate_month = dismissalDate_month;
+                this.dateOfDismiss_month = dateOfDismiss_month;
             }
         } catch (Exception e) {
             System.out.println("Please enter just digit!");
             Toolkit.getDefaultToolkit().beep();
-            setDismissalDate_month(1);
+            setDateOfDismiss_month(1);
         }
     }
 
-    public int getDismissalDate_year() {
-        return dismissalDate_year;
+    public int getDateOfDismiss_year() {
+        return dateOfDismiss_year;
     }
 
-    public void setDismissalDate_year(int dismissalDate_year) {
+    public void setDateOfDismiss_year(int dateOfDismiss_year) {
         try {
             System.out.println("Please enter date of dismissal year : ");
             String sYear = keyboard.nextLine();
-            dismissalDate_year = Integer.parseInt(sYear);
+            dateOfDismiss_year = Integer.parseInt(sYear);
             if (!sYear.matches(".*[0-9].*")) {
                 System.out.println("Please use just digits.");
                 Toolkit.getDefaultToolkit().beep();
-                setDismissalDate_year(1);
-            } else if (dismissalDate_year < 2000) {
+                setDateOfDismiss_year(1);
+            } else if (dateOfDismiss_year < 2000) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_year = 2000;
-            } else if (dismissalDate_year >= Year.now().getValue()) {
+                this.dateOfDismiss_year = 2000;
+            } else if (dateOfDismiss_year >= Year.now().getValue()) {
                 Toolkit.getDefaultToolkit().beep();
-                this.dismissalDate_year = Year.now().getValue();
+                this.dateOfDismiss_year = Year.now().getValue();
             } else {
-                this.dismissalDate_year = dismissalDate_year;
+                this.dateOfDismiss_year = dateOfDismiss_year;
             }
         } catch (Exception e) {
             System.out.println("Please enter just digit!");
             Toolkit.getDefaultToolkit().beep();
-            setDismissalDate_year(1);
+            setDateOfDismiss_year(1);
         }
     }
+
+    public Employee() {
+    }
+
+    public Employee(String employeeId, String department, int dateOfStart_day, int dateOfStart_month, int dateOfStart_year,
+                    int dateOfDismiss_day, int dateOfDismiss_month, int dateOfDismiss_year) {
+
+        this.employeeId = employeeId;
+        this.department = department;
+        this.dateOfStart_day = dateOfStart_day;
+        this.dateOfStart_month = dateOfStart_month;
+        this.dateOfStart_year = dateOfStart_year;
+        this.dateOfDismiss_day = dateOfDismiss_day;
+        this.dateOfDismiss_month = dateOfDismiss_month;
+        this.dateOfDismiss_year = dateOfDismiss_year;
+    }
+
+    public String toString() {
+
+        return  super.person.toString() + "Employee ID : " + getEmployeeId() + "\nDepartment : " + getDepartment() +
+                "\nDate of Start : " + getDateOfStart_day() + "/" + getDateOfStart_month() + "/" + getDateOfStart_year() +
+                "\nDate of Dismiss : " + getDateOfDismiss_day() +"/"+ getDateOfDismiss_month()+ "/" + getDateOfDismiss_year();
+    }public void addEmployee() {
+        super.addPerson();
+        setEmployeeId("Unknown");
+        setDepartment("Unknown");
+        setDateOfStart_day(1);
+        setDateOfStart_month(1);
+        setDateOfStart_year(1);
+        setDateOfDismiss_day(1);
+        setDateOfDismiss_month(1);
+        setDateOfDismiss_year(1);
+    }
+
+    public void viewEmployee() {
+        System.out.println(super.toString()+toString());
+    }
+
+    public void changeEmployeeId() {
+        setEmployeeId("Unknown");
+        System.out.println("New Employee ID is " + getEmployeeId());
+    }
+
+    public void changeDepartment() {
+        setDepartment("Unknown");
+        System.out.println("New Department is " + getDepartment());
+    }
+
+    public void changeDateOfStart_day() {
+        setDateOfStart_day(0);
+        System.out.println("New DAY is " + getDateOfStart_day());
+    }
+
+    public void changeDateOfStart_month() {
+        setDateOfStart_month(0);
+        System.out.println("New MONTH is " + getDateOfStart_month());
+    }
+
+    public void changeDateOfStart_year() {
+        setDateOfStart_year(0);
+        System.out.println("New YEAR is " + getDateOfStart_year());
+    }
+    public void changeDateOfDismiss_day() {
+        setDateOfDismiss_day(0);
+        System.out.println("New DAY is " + getDateOfDismiss_day());
+    }
+
+    public void changeDateOfDismiss_month() {
+        setDateOfDismiss_month(0);
+        System.out.println("New MONTH is " + getDateOfDismiss_month());
+    }
+
+    public void changeDateOfDismiss_year() {
+        setDateOfDismiss_year(0);
+        System.out.println("New YEAR is " + getDateOfDismiss_year());
+    }
+
 }
