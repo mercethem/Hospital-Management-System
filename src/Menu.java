2ae7014f230b5ec99d865d2c5bde62239a4bf0d6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-26 17:13:15 +0300

    object fixed

--- src/Menu.java
+++ src/Menu.java
@@ -6,7 +6,7 @@ public class Menu {
             1990, "A rh +", "Üniversite", "5424974673", "mercethem@gmail.com");
 
     static Employee employee = new Employee("220541022", "Software Engineering",
-            1, 1, 2022, 0, 0, 2023);
+            1, 1, 2022, 1, 1, 2023);
 
     static Patient patient = new Patient("Neurosurgery",
             10, 10, 2021,
@@ -57,30 +57,39 @@ public class Menu {
                 options();
             }
         } else if (choose == 2) {
-            choose = keyboard.nextInt();
             System.out.println("For Person(0)\nFor Employee(1)\nFor Patient(2)\n");
+            choose = keyboard.nextInt();
             if (choose == 0) {
                 System.out.println("Please enter what do you want to change :\n" +
                         "Name(0)\nSurname(1)\nDay(2)\nMonth(3)\nYear(4)\nBloodGroup(5)\nAddress(6)\nPhoneNumber(7)\nEmail(8)");
                 choose = keyboard.nextInt();
                 if (choose == 0) {
                     person.changeName();
+                    options();
                 } else if (choose == 1) {
                     person.changeSurname();
+                    options();
                 } else if (choose == 2) {
                     person.changeBirthdate_day();
+                    options();
                 } else if (choose == 3) {
                     person.changeBirthdate_month();
+                    options();
                 } else if (choose == 4) {
                     person.changeBirthdate_year();
+                    options();
                 } else if (choose == 5) {
                     person.changeBloodGroup();
+                    options();
                 } else if (choose == 6) {
                     person.changeAddress();
+                    options();
                 } else if (choose == 7) {
                     person.changePhoneNumber();
+                    options();
                 } else if (choose == 8) {
                     person.changeEmail();
+                    options();
                 } else {
                     System.out.println("Please enter right number!");
                     Toolkit.getDefaultToolkit().beep();
@@ -93,20 +102,28 @@ public class Menu {
                 choose = keyboard.nextInt();
                 if (choose == 0) {
                     employee.changeEmployeeId();
+                    options();
                 } else if (choose == 1) {
                     employee.changeDepartment();
+                    options();
                 } else if (choose == 2) {
                     employee.changeDateOfStart_day();
+                    options();
                 } else if (choose == 3) {
                     employee.changeDateOfStart_month();
+                    options();
                 } else if (choose == 4) {
                     employee.changeDateOfStart_year();
+                    options();
                 } else if (choose == 5) {
                     employee.changeDateOfDismiss_day();
+                    options();
                 } else if (choose == 6) {
                     employee.changeDateOfDismiss_month();
+                    options();
                 } else if (choose == 7) {
                     employee.changeDateOfDismiss_year();
+                    options();
                 } else {
                     System.out.println("Please enter right number!");
                     Toolkit.getDefaultToolkit().beep();
@@ -119,18 +136,25 @@ public class Menu {
                 choose = keyboard.nextInt();
                 if (choose == 0) {
                     patient.changeDepartment();
+                    options();
                 } else if (choose == 1) {
                     patient.changeDateOfHospitalized_day();
+                    options();
                 } else if (choose == 2) {
                     patient.changeDateOfHospitalized_month();
+                    options();
                 } else if (choose == 3) {
                     patient.changeDateOfHospitalized_year();
+                    options();
                 } else if (choose == 4) {
                     patient.changeDateOfLeave_day();
+                    options();
                 } else if (choose == 5) {
                     patient.changeDateOfLeave_month();
+                    options();
                 } else if (choose == 6) {
                     patient.changeDateOfLeave_year();
+                    options();
                 } else {
                     System.out.println("Please enter right number!");
                     Toolkit.getDefaultToolkit().beep();
