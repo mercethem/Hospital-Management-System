b32581d01e0363e708d40947504cc2b5f4435826
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-04-29 01:15:39 +0300

    initial commit

---
+++ src/DataBaseLayer.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseLayer {
+    static Connection myConnection;
+    static String url = "jdbc:sqlserver://localhost\\DESKTOP-T9F59R5:1433;database=HospitalManagementSystemStock;useSSL=false;encrypt=false;integratedSecurity=true;"; // Database URL
+
+    public static void dataBaseLayer() { //Database layer function
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Driver name
+            myConnection = DriverManager.getConnection(url);
+            System.out.println("Connections is successful!");
+        } catch (Exception e) {
+            System.out.println("Connection is failed!");
+        }
+    }
+}
