fc52ad1d00783013780bead8155f70dd4cad314f
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-05-16 16:21:17 +0300

    reformat and department value fixed

--- src/Management.java
+++ src/Management.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -7,8 +8,7 @@ public class Management extends Employee {
     Scanner keyboard = new Scanner(System.in);
     final private int MANAGEMENT_ID_DIGIT = 9;
     private String managementId;
-    private String department;
-
+    private String management_department;
 
 
     public String getManagementId() {
@@ -26,7 +26,7 @@ public class Management extends Employee {
                     DataBaseLayer.dataBaseLayer();
                     Statement myStatement = DataBaseLayer.myConnection.createStatement();
                     myStatement.executeQuery("IF ( '" + super.getEmployeeId() + "' =(SELECT employeeId FROM employees WHERE employeeId = '" + super.getEmployeeId() + "') )\n" +
-                            "    UPDATE HospitalManagementSystemStock.dbo.managements SET  managemenId = '" + this.managementId + "' WHERE employeeId = '" + super.getEmployeeId() + "'\n");
+                            "    UPDATE HospitalManagementSystemStock.dbo.managements SET  managementId = '" + this.managementId + "' WHERE employeeId = '" + super.getEmployeeId() + "'\n");
                     myStatement.close(); // close statement
                     DataBaseLayer.myConnection.close(); // close connection
                 } catch (Exception e) {
@@ -43,33 +43,34 @@ public class Management extends Employee {
     }
 
 
-    public String getDepartment() {
-        return department;
+    public String getManagement_department() {
+        return management_department;
     }
-    public void setDepartment(String  department) {
+
+    public void setManagement_department(String employee_department) {
         int count = 0;
         System.out.println("Please enter department : ");
-        department = keyboard.nextLine();
+        management_department = keyboard.nextLine();
 
-        if (department.length() < 2) {
+        if (management_department.length() < 2) {
             System.out.println("Please enter your true department!");
             Toolkit.getDefaultToolkit().beep();
             ++count;
-        } else if (department.matches(".*\\p{Punct}.*")) {
+        } else if (management_department.matches(".*\\p{Punct}.*")) {
             System.out.println("Please do not use punctuation character!");
             Toolkit.getDefaultToolkit().beep();
             ++count;
-        } else if (department.matches(".*[0-9].*")) {
+        } else if (management_department.matches(".*[0-9].*")) {
             System.out.println("Please do not use digit!");
             Toolkit.getDefaultToolkit().beep();
             ++count;
         } else {
-            this.department = department;
+            this.management_department = management_department;
             try {
                 DataBaseLayer.dataBaseLayer();
                 Statement myStatement = DataBaseLayer.myConnection.createStatement();
                 myStatement.executeQuery("IF ( '" + super.getEmployeeId() + "' =(SELECT employeeId FROM employees WHERE employeeId = '" + super.getEmployeeId() + "') )\n" +
-                        "    UPDATE HospitalManagementSystemStock.dbo.managements SET  department = '" + this.department + "' WHERE employeeId = '" + super.getEmployeeId() + "'\n");
+                        "    UPDATE HospitalManagementSystemStock.dbo.managements SET  department = '" + this.management_department + "' WHERE employeeId = '" + super.getEmployeeId() + "'\n");
                 myStatement.close(); // close statement
                 DataBaseLayer.myConnection.close(); // close connection
             } catch (Exception e) {
@@ -77,11 +78,69 @@ public class Management extends Employee {
             }
         }
         if (count == 1) {
-            setDepartment("");
+            setManagement_department("");
         }
 
 
     }
 
 
+    public Management() {
+    }
+
+    public Management(String managementId, String management_department) {
+
+        this.managementId = managementId;
+        this.management_department = management_department;
+    }
+
+    public String toString() {
+        management_db();
+        return super.toString() + "\nMANAGEMENT INFORMATION" + "\n--------------------------------------------\n"
+                + "Management ID : " + getManagementId() + "\nDepartment : " + getManagement_department() +
+                "\n--------------------------------------------\n";
+
+    }
+
+    public void management_db() {
+        super.person_db();
+        super.employee_db();
+        String employeeId = getEmployeeId();
+
+        try {
+            DataBaseLayer.dataBaseLayer();
+            Statement myStatement = DataBaseLayer.myConnection.createStatement();
+            ResultSet myResult = myStatement.executeQuery("SELECT * FROM  managements WHERE employeeId = '" + employeeId + "'");
+//            "IF ( '"+getEmployeeId()+"' = (SELECT employeeId FROM HospitalManagementSystemStock.dbo.managements WHERE employeeId = '"+getEmployeeId()+"') " +
+//                    "SELECT * FROM  managements WHERE employeeId = '"+getEmployeeId()+"'"
+            while (myResult.next()) {
+                this.managementId = myResult.getString("managementId");
+                this.management_department = myResult.getString("department");
+            }
+            myStatement.close(); // close statement
+            DataBaseLayer.myConnection.close(); // close connection
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void addManagement() {
+        super.addEmployee();
+        setManagementId("Unknown");
+        setManagement_department("Unknown");
+    }
+
+    public void viewManagement() {
+        System.out.println(toString());
+    }
+
+    public void changeManagementId() {
+        setManagementId("Unknown");
+        System.out.println("New Management ID is " + getEmployeeId());
+    }
+
+    public void changeEmployeeDepartment() {
+        setManagement_department("Unknown");
+        System.out.println("New Department is " + getManagement_department());
+    }
 }
